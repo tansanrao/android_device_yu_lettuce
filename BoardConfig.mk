845c38a2801956a685e63d0e524f126d401b3712
Tanuj Rao <tanujrao99@gmail.com>
2017-02-17 03:32:34 +0000

    Use SnapDragon LLVM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,3 +19,13 @@ include device/yu/lettuce/board/*.mk
 
 # inherit from proprietary files
 -include vendor/yu/lettuce/BoardConfigVendor.mk
+
+ifneq ($(HOST_OS),darwin)
+
+SDCLANG := true
+
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
+
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+
+endif
